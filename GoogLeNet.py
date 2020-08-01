0868f9f24d68efb044c2ef5d3c9bc827ea7a6a47
허재섭 <shjas94@outlook.kr>
2020-08-01 19:19:20 +0900

    GoogleNet 수정

--- GoogLeNet.py
+++ GoogLeNet.py
@@ -3,20 +3,28 @@ import tensorflow as tf
 
 
 class InceptionModule(tf.keras.Model):
-    def __init__(self, filter_outs, activation='relu'): # 각 size별 출력 list로
+    def __init__(self, filter_outs, activation='relu'):  # 각 size별 출력 list로
         super(InceptionModule, self).__init__()
-        self.conv1_1 = tf.keras.layers.Conv2D(32 ,kernel_size=(1,1), padding='same', activation=activation)
-        self.conv3 = tf.keras.layers.Conv2D(filter_outs[0], kernel_size=(3,3), padding='same', activation=activation)
+        self.conv1_1 = tf.keras.layers.Conv2D(filter_outs[0], kernel_size=(
+            1, 1), padding='same', activation=activation)
+        self.conv3 = tf.keras.layers.Conv2D(filter_outs[1], kernel_size=(
+            3, 3), padding='same', activation=activation)
 
-        self.conv1_2 = tf.keras.layers.Conv2D(32, kernel_size=(1,1), padding='same', activation=activation)
-        self.conv5 = tf.keras.layers.Conv2D(filter_outs[1], kernel_size=(3,3), padding='same', activation=activation)
-        
-        self.maxpool = tf.keras.layers.MaxPool2D(pool_size=(3,3), strides=(1,1), padding='same', activation=activation)
-        self.conv1_3 = tf.keras.layers.Conv2D(filter_outs[2], kernel_size=(1,1), padding='same', activation=activation)
+        self.conv1_2 = tf.keras.layers.Conv2D(filter_outs[2], kernel_size=(
+            1, 1), padding='same', activation=activation)
+        self.conv5 = tf.keras.layers.Conv2D(filter_outs[3], kernel_size=(
+            5, 5), padding='same', activation=activation)
 
-        self.conv1_4 = tf.keras.layers.Conv2D(filter_outs[3], kernel_size=(1,1), padding='same', activation=activation)
+        self.maxpool = tf.keras.layers.MaxPool2D(pool_size=(
+            3, 3), strides=(1, 1), padding='same', activation=activation)
+        self.conv1_3 = tf.keras.layers.Conv2D(filter_outs[4], kernel_size=(
+            1, 1), padding='same', activation=activation)
+
+        self.conv1_4 = tf.keras.layers.Conv2D(filter_outs[5], kernel_size=(
+            1, 1), padding='same', activation=activation)
 
         self.concat = tf.keras.layers.Concatenate()
+
     def call(self, x):
         op1 = self.conv1_1(x)
         op1 = self.conv3(x)
