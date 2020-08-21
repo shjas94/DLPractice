d197cc465e614a1c4af44b691f66b11ba75e8f15
허재섭 <shjas94@outlook.kr>
2020-08-21 14:03:27 +0900

    SE_ResNet update

--- SE_ResNet.py
+++ SE_ResNet.py
@@ -26,7 +26,7 @@ class SEBlock(tf.keras.layers.Layer):
 
 
 class SEResidualBlock(tf.keras.layers.Layer):
-    def __init__(self, channel_in, channel_out, kernel_size, ratio, regularizer=None, **kwargs):
+    def __init__(self, channel_in, channel_out, kernel_size, reduction_ratio=16, regularizer=None, **kwargs):
         super().__init__(**kwargs)
         self.bn1 = tf.keras.layers.BatchNormalization()
         self.ac1 = tf.nn.swish()
@@ -36,7 +36,7 @@ class SEResidualBlock(tf.keras.layers.Layer):
         self.ac2 = tf.nn.swish()
         self.conv2 = tf.keras.layers.Conv2D(
             channel_out, kernel_size=kernel_size, kernel_regularizer=regularizer, kernel_initializer='he_normal', padding='same')
-        self.se = SEBlock(channel_out, 16)
+        self.se = SEBlock(channel_out, reduction_ratio)
         if channel_in == chennel_out:
             self.identity = lambda x: x
         else:
@@ -55,21 +55,21 @@ class SEResidualBlock(tf.keras.layers.Layer):
 
 
 class SEDeepResidualBlock(tf.keras.layers.Layer):
-    def __init__(self, channel_in, channel_out, kernel_size, regularizer=None, reduction_ratio=0.3, **kwargs):
+    def __init__(self, channel_in, channel_out, kernel_size, regularizer=None, reduction_ratio=16, bottleneck_ratio=0.4, **kwargs):
         super().__init__(**kwargs)
         self.bn1 = tf.keras.layers.BatchNormalization()
         self.ac1 = tf.nn.swish()
-        self.conv1 = tf.keras.layers.Conv2D(tf.math.floor(channel_out*reduction_ratio), kernel_size=(
+        self.conv1 = tf.keras.layers.Conv2D(tf.math.floor(channel_out*bottleneck_ratio), kernel_size=(
             1, 1), kernel_regularizer=regularizer, kernel_initializer='he_normal', padding='same')
         self.bn2 = tf.keras.layers.BatchNormalization()
         self.ac2 = tf.nn.swish()
-        self.conv2 = tf.keras.layers.Conv2D(tf.math.floor(channel_out*reduction_ratio), kernel_size=kernel_size,
+        self.conv2 = tf.keras.layers.Conv2D(tf.math.floor(channel_out*bottleneck_ratio), kernel_size=kernel_size,
                                             kernel_regularizer=regularizer, kernel_initializer='he_normal', padding='same')
         self.bn3 = tf.keras.layers.BatchNormalization()
         self.ac3 = tf.nn.swish()
         self.conv3 = tf.keras.layers.Conv2D(channel_out, kernel_size=(
             1, 1), kernel_regularizer=regularizer, kernel_initializer='he_normal', padding='same')
-        self.se = SEBlock(channel_out, 16)
+        self.se = SEBlock(channel_out, reduction_ratio)
         if channel_in == channel_out:
             self.identity = lambda x: x
         else:
@@ -87,7 +87,3 @@ class SEDeepResidualBlock(tf.keras.layers.Layer):
         h = self.conv3(h)
         h = self.se(h)
         return self.identity(x) + h
-
-
-class SEResNet(tf.keras.Model):
-    pass
