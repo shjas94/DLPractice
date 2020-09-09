6a08fa42c69d2d414090010699bfab20323b4a85
허재섭 <shjas94@outlook.kr>
2020-09-09 20:46:20 +0900

    Reduction 모듈 추가

--- InceptionV4.py
+++ InceptionV4.py
@@ -259,3 +259,118 @@ class Inception_B(tf.keras.layers.Layer):
         config.update({"filter_outs": self.filter_outs,
                        "residual": self.residual})
         return config
+
+
+class Inception_C(tf.keras.layers.Layer):
+    def __init__(self, filter_outs, residual=False, **kwargs):
+        super().__init__(**kwargs)
+        ## HyperParameters##
+        self.filter_outs = filter_outs
+        self.residual = residual
+        ####################
+        if residual:
+            pass
+        else:
+            self.avgpool = tf.keras.layers.AveragePooling2D()
+            self.conv1_1 = tf.keras.layers.Conv2D(self.filter_outs[0], kernel_size=(
+                1, 1), padding='same', activation='elu', kernel_initializer='he_normal')
+
+            self.conv2_1 = tf.keras.layers.Conv2D(self.filter_outs[1], kernel_size=(
+                1, 1), padding='same', activation='elu', kernel_initializer='he_normal')
+
+            self.conv3_1 = tf.keras.layers.Conv2D(self.filter_outs[2]+128, kernel_size=(
+                1, 1), padding='same', activation='elu', kernel_initializer='he_normal')
+            self.conv3_2_1 = tf.keras.layers.Conv2D(self.filter_outs[2], kernel_size=(
+                1, 3), padding='same', activation='elu', kernel_initializer='he_normal')
+            self.conv3_2_2 = tf.keras.layers.Conv2D(self.filter_outs[2], kernel_size=(
+                3, 1), padding='same', activation='elu', kernel_initializer='he_normal')
+
+            self.conv4_1 = tf.keras.layers.Conv2D(self.filter_outs[3]+128, kernel_size=(
+                1, 1), padding='same', activation='elu', kernel_initializer='he_normal')
+            self.conv4_2 = tf.keras.layers.Conv2D(self.filter_outs[3]+192, kernel_size=(
+                1, 3), padding='same', activation='elu', kernel_initializer='he_normal')
+            self.conv4_3 = tf.keras.layers.Conv2D(self.filter_outs[3]+256, kernel_size=(
+                3, 1), padding='same', activation='elu', kernel_initializer='he_normal')
+            self.conv4_4_1 = tf.keras.layers.Conv2D(self.filter_outs[3], kernel_size=(
+                3, 1), padding='same', activation='elu', kernel_initializer='he_normal')
+            self.conv4_4_2 = tf.keras.layers.Conv2D(self.filter_outs[3], kernel_size=(
+                1, 3), padding='same', activation='elu', kernel_initializer='he_normal')
+
+            self.concat = tf.keras.layers.Concatenate()
+
+    def call(self, x):
+        if self.residual:
+            pass
+
+        else:
+            h1 = self.avgpool(x)
+            h1 = self.conv1_1(h1)
+
+            h2 = self.conv2_1(x)
+
+            h3 = self.conv3_1(x)
+            h3_1 = self.conv3_2_1(h3)
+            h3_2 = self.conv3_2_2(h3)
+
+            h4 = self.conv4_1(x)
+            h4 = self.conv4_2(h4)
+            h4 = self.conv4_2(h4)
+            h4_1 = self.conv4_4_1(h4)
+            h4_2 = self.conv4_4_2(h4)
+
+            return self.concat([h1, h2, h3_1, h3_2, h4_1, h4_2])
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"filter_outs": self.filter_outs,
+                       "residual": self.residual})
+        return config
+
+
+class Reduction_1(tf.keras.layers.Layer):
+    def __init__(self, k, l, m, n, residual=False, **kwargs):
+        super().__init__(**kwargs)
+        ## HyperParameters##
+        self.k = k
+        self.l = l
+        self.m = m
+        self.n = n
+        self.residual = residual
+        ####################
+        if residual == True:
+            pass
+        else:
+            self.pool1 = tf.keras.layers.MaxPool2D(
+                (3, 3), strides=2, padding='valid')
+
+            self.conv2 = tf.keras.layers.Conv2D(self.n, kernel_size=(3, 3), strides=(
+                2, 2), padding='valid', activation='elu', kernel_initializer='he_normal')
+
+            self.conv3_1 = tf.keras.layers.Conv2D(self.k, kernel_size=(
+                1, 1), padding='same', activation='elu', kernel_initializer='he_normal')
+            self.conv3_2 = tf.keras.layers.Conv2D(self.l, kernel_size=(
+                3, 3), padding='same', activation='elu', kernel_initializer='he_normal')
+            self.conv3_3 = tf.keras.layers.Conv2D(self.m, kernel_size=(
+                3, 3), padding='valid', strides=2, activation='elu', kernel_initializer='he_normal')
+
+            self.concat = tf.keras.layers.Concatenate()
+
+    def call(self, x):
+        if residual == True:
+            pass
+        else:
+            h1 = self.pool1(x)
+
+            h2 = self.conv2(x)
+
+            h3 = self.conv3_1(x)
+            h3 = self.conv3_2(h3)
+            h3 = self.conv3_3(h3)
+
+            return self.concat([h1, h2, h3])
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"k": self.k, "l": self.l, "m": self.m,
+                       "n": self.n, "residual": self.residual})
+        return config
