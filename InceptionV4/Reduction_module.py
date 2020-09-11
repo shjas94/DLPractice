bfe0cb43a9d253cd8327db4a1f527c72631e4106
허재섭 <shjas94@outlook.kr>
2020-09-11 17:07:11 +0900

    reduction_2 추가

--- InceptionV4/Reduction_module.py
+++ InceptionV4/Reduction_module.py
@@ -48,3 +48,48 @@ class Reduction_1(tf.keras.layers.Layer):
         config.update({"k": self.k, "l": self.l, "m": self.m,
                        "n": self.n, "residual": self.residual})
         return config
+
+
+class Reduction_2(tf.keras.layers.Layer):
+    def __init__(self, filter_outputs, **kwargs):
+        super().__init__(**kwargs)
+        ## HyperParameters##
+        self.filter_outputs = filter_outputs
+        ####################
+
+        self.pool = tf.keras.layers.MaxPool2D(
+            kernel_size=(3, 3), strides=2, padding='valid')
+
+        self.conv2_1 = tf.keras.layers.Conv2D(self.filter_outputs[0], kernel_size=(
+            1, 1), padding='same', activation='elu', kernel_initializer='he_normal')
+        self.conv2_2 = tf.keras.layers.Conv2D(self.filter_outputs[0], kernel_size=(
+            3, 3), padding='valid', strides=2, activation='elu', kernel_initializer='he_normal')
+
+        self.conv3_1 = tf.keras.layers.Conv2D(self.filter_outputs[1]-100, kernel_size=(
+            1, 1), padding='same', activation='elu', kernel_initializer='he_normal')
+        self.conv3_2 = tf.keras.layers.Conv2D(self.filter_outputs[1]-100, kernel_size=(
+            1, 7), padding='same', activation='elu', kernel_initializer='he_normal')
+        self.conv3_3 = tf.keras.layers.Conv2D(self.filter_outputs[1], kernel_size=(
+            7, 1), padding='same', activation='elu', kernel_initializer='he_normal')
+        self.conv3_4 = tf.keras.layers.Conv2D(self.filter_outputs[1], kernel_size=(
+            3, 3), padding='valid', strides=2, activation='elu', kernel_initializer='he_normal')
+
+        self.concat = tf.keras.layers.Concatenate()
+
+    def call(self, x):
+        h1 = self.pool(x)
+
+        h2 = self.conv2_1(x)
+        h2 = self.conv2_2(h2)
+
+        h3 = self.conv3_1(x)
+        h3 = self.conv3_2(h3)
+        h3 = self.conv3_3(h3)
+        h3 = self.conv3_4(h3)
+
+        return self.concat([h1, h2, h3])
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"filter_outputs": self.filter_outputs})
+        return config
